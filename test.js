2c638279f37f8afa0d633c93c862ecf80337913c
Alokam Chinenye Augusta <alokamaugusta@gmail.com>
2022-07-23 13:40:42 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,9 @@
+// to print out anything in Javascript 
+console.log('Hello World')
+
+//let, const
+//let is used to declare a variable. Let allows the variable to be reassigned
+let name = "Augusta"
+
+
+
